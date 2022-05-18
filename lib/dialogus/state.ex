faf1ceed6083ef5ff2da3da0e9e0487e94b3c6ca
Manlio Joaquín García González <info@manliogarcia.es>
2022-05-18 13:03:21 +0100

    Generalize state management

--- lib/dialogus/state.ex
+++ lib/dialogus/state.ex
@@ -1,17 +1,25 @@
 defmodule Dialogus.State do
   use Supervisor
 
-  def subscribe,
-    do: Phoenix.PubSub.subscribe(Dialogus.PubSub, "#{__MODULE__}")
+  @pubsub Module.concat([__MODULE__, "PubSub"])
 
-  def broadcast(message) do
-    Phoenix.PubSub.broadcast(Dialogus.PubSub, "#{__MODULE__}", message)
-    message
+  def subscribe,
+    do: Phoenix.PubSub.subscribe(@pubsub, "#{__MODULE__}")
+
+  def broadcast(payload) when is_tuple(payload) do
+    Phoenix.PubSub.broadcast(@pubsub, "#{__MODULE__}", payload)
+    payload
   end
 
-  def broadcast(message, action) do
-    Phoenix.PubSub.broadcast(Dialogus.PubSub, "#{__MODULE__}", {action, message})
-    message
+  def broadcast(payload, event \\ nil, origin \\ __MODULE__)
+      when is_atom(event) and is_atom(origin) do
+    if event do
+      Phoenix.PubSub.broadcast(@pubsub, "#{__MODULE__}", {origin, event, payload})
+    else
+      Phoenix.PubSub.broadcast(@pubsub, "#{__MODULE__}", {origin, payload})
+    end
+
+    payload
   end
 
   def start_link(init_arg),
@@ -19,13 +27,122 @@ defmodule Dialogus.State do
 
   @impl true
   def init(_init_arg) do
-    children = [
-      Dialogus.State.Messages,
-      Dialogus.State.Stats,
-      Dialogus.State.Info,
-      Dialogus.State.Persistence
-    ]
+    children =
+      with {:ok, modules} <- :application.get_key(:dialogus, :modules) do
+        modules
+        |> Enum.filter(&("#{&1}" != "#{__MODULE__}"))
+        |> Enum.filter(&String.starts_with?("#{&1}", "#{__MODULE__}."))
+        |> Enum.filter(&Keyword.has_key?(&1.__info__(:functions), :start_link))
+      end
 
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.init([{Phoenix.PubSub, name: @pubsub} | children],
+      strategy: :one_for_one
+    )
+  end
+
+  defmacro report(payload) do
+    quote do
+      unquote(__MODULE__).broadcast(unquote(payload), elem(__ENV__.function, 0), __MODULE__)
+      send(self(), {__MODULE__, elem(__ENV__.function, 0), unquote(payload)})
+      unquote(payload)
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      use GenServer
+
+      alias unquote(__MODULE__), as: State
+      import unquote(__MODULE__), only: [report: 1]
+
+      defp persistence_path do
+        filename =
+          "#{__MODULE__}"
+          |> String.replace_prefix("#{unquote(__MODULE__)}.", "")
+          |> String.split(".")
+          |> Enum.map(&Macro.underscore/1)
+          |> Enum.join("-")
+
+        "_state_persistence/#{filename}.dat"
+      end
+
+      defp set_persistence(state \\ nil) do
+        payload = state || get()
+
+        with :ok <- File.mkdir_p(Path.dirname(persistence_path())),
+             do: File.write(persistence_path(), :erlang.term_to_binary(payload))
+
+        payload
+      end
+
+      defp get_persistence do
+        case File.read(persistence_path()) do
+          {:ok, binary} -> :erlang.binary_to_term(binary)
+          {:error, _} -> %{}
+        end
+      end
+
+      def start_link(args \\ []) do
+        state = Map.merge(Keyword.get(args, :state, %{}), get_persistence())
+
+        with {:ok, pid} <-
+               GenServer.start_link(__MODULE__, state, name: __MODULE__) do
+          State.broadcast(state, :start_link, __MODULE__)
+          {:ok, pid}
+        else
+          error -> error
+        end
+      end
+
+      @persist_tick 1000 * 30
+
+      def init(state) do
+        Process.flag(:trap_exit, true)
+        State.subscribe()
+        Process.send_after(self(), {:persist, @persist_tick}, @persist_tick)
+        {:ok, report(state)}
+      end
+
+      def get,
+        do: GenServer.call(__MODULE__, :get)
+
+      def get(key),
+        do: GenServer.call(__MODULE__, {:get, key})
+
+      def set(state),
+        do: GenServer.cast(__MODULE__, {:set, state})
+
+      def set(key, value),
+        do: GenServer.cast(__MODULE__, {:set, key, value})
+
+      def unset(key),
+        do: GenServer.cast(__MODULE__, {:unset, key})
+
+      def handle_call(:get, _from, state),
+        do: {:reply, state, state}
+
+      def handle_call({:get, key}, _from, state),
+        do: {:reply, state[key], state}
+
+      def handle_cast({:set, new_state}, _old_state),
+        do: {:noreply, report(new_state)}
+
+      def handle_cast({:unset, key}, state),
+        do: {:noreply, report(Map.delete(state, key))}
+
+      def handle_cast({:set, key, nil}, state),
+        do: {:noreply, report(Map.delete(state, key))}
+
+      def handle_cast({:set, key, value}, state),
+        do: {:noreply, report(Map.put(state, key, value))}
+
+      def handle_info({:EXIT, _pid, _reason}, state),
+        do: {:noreply, set_persistence(state)}
+
+      def handle_info({:persist, tick}, state) do
+        Process.send_after(self(), :persist, tick)
+        {:noreply, set_persistence(state)}
+      end
+    end
   end
 end
